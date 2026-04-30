85e16ecb8d829c4b662efcbf5826e7630d44afc8
akshay <akshaynangia123@gmail.com>
2026-04-30 12:25:48 -0500

    changed and fixed ya mom

--- ConsoleApp1/Card.cs
+++ ConsoleApp1/Card.cs
@@ -29,7 +29,14 @@ public class Card
 {
     public Suit Suit { get; private set; }
     public Rank Rank { get; private set; }
-    public int BlackjackValue => (int)Rank;
+
+    public int BlackjackValue
+    {
+        get
+        {
+            return (int)Rank;
+        }
+    }
 
     public Card(Suit suit, Rank rank)
     {
@@ -37,39 +44,77 @@ public class Card
         Rank = rank;
     }
 
-    public override string ToString() => $"{Rank} of {Suit}";
+    public override string ToString()
+    {
+        return Rank + " of " + Suit;
+    }
 }
 
 public class Deck
 {
     private Queue<Card> _cards = new Queue<Card>();
 
-    public int Count => _cards.Count;
+    public int Count
+    {
+        get
+        {
+            return _cards.Count;
+        }
+    }
 
     public Deck(int numberOfDecks = 1)
     {
         for (int d = 0; d < numberOfDecks; d++)
+        {
             foreach (Suit suit in Enum.GetValues(typeof(Suit)))
-            foreach (Rank rank in Enum.GetValues(typeof(Rank)))
-                _cards.Enqueue(new Card(suit, rank));
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    Card card = new Card(suit, rank);
+                    _cards.Enqueue(card);
+                }
+            }
+        }
     }
 
-    // Fisher-Yates shuffle before queuing
     public void Shuffle()
     {
-        var list = new System.Collections.Generic.List<Card>(_cards);
-        var rng = new Random();
+        List<Card> list = new List<Card>(_cards);
+        Random rng = new Random();
+
         for (int i = list.Count - 1; i > 0; i--)
         {
             int j = rng.Next(i + 1);
-            (list[i], list[j]) = (list[j], list[i]);
+
+            Card temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
         }
+
         _cards = new Queue<Card>(list);
     }
 
-    public Card Deal() =>
-        _cards.Count > 0 ? _cards.Dequeue() : throw new InvalidOperationException("Deck is empty.");
+    public Card Deal()
+    {
+        if (_cards.Count > 0)
+        {
+            return _cards.Dequeue();
+        }
+        else
+        {
+            throw new InvalidOperationException("Deck is empty.");
+        }
+    }
 
-    public Card Peek() =>
-        _cards.Count > 0 ? _cards.Peek() : throw new InvalidOperationException("Deck is empty.");
+    public Card Peek()
+    {
+        if (_cards.Count > 0)
+        {
+            return _cards.Peek();
+        }
+        else
+        {
+            throw new InvalidOperationException("Deck is empty.");
+        }
+    }
 }
